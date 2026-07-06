07c57bb6ecf1cf1fa249cfc2186393deb081078e
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-07-06 16:49:30 +0300

    feat: add keyDown event

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+interface Props {}
+interface State {
+  pressedKey: string;
+}
+
+export class App extends React.PureComponent<Props, State> {
+  state: State = {
+    pressedKey: '',
+  };
+
+  onKeyDown = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.onKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.onKeyDown);
+  }
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <>
+        <div className="App">The last pressed key is [Enter]</div>
+        {pressedKey ? (
+          <p className="App__message">The last pressed key is [{pressedKey}]</p>
+        ) : (
+          <p className="App__message">Nothing was pressed yet</p>
+        )}
+      </>
+    );
+  }
+}
